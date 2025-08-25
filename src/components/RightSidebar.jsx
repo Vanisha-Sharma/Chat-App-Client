87c6169a2f671b4aad4ec6fcf418bc01f14c7a0a
Vanisha Sharma <sharmavanisha2005@gmail.com>
2025-08-26 03:00:57 +0530

    Add files via upload

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -25,9 +25,10 @@ const RightSidebar = () => {
         <div className="pt-16 flex flex-col items-center gap-2 text-xs font-light mx-auto">
           <img
             src={selectedUser.profilePic || assets.avatar_icon}
-            alt=""
-            className="w-20 aspect-[1/1] rounded-full"
+            alt="profile"
+            className="w-20 h-20 rounded-full object-cover"
           />
+
           <h1 className="px-10 text-xl font-medium mx-auto flex items-center gap-2">
             {onlineUsers.includes(selectedUser._id) && (
               <span className="w-2 h-2 rounded-full bg-green-500"></span>
